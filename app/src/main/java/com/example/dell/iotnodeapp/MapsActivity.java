f5ab3561addfc260fa5300d0814be575081f9bc6
safa-ads <safa.mohamed.ads@gmail.com>
2016-06-30 21:49:06 +0300

    more pins added to the map

--- app/src/main/java/com/example/dell/iotnodeapp/MapsActivity.java
+++ app/src/main/java/com/example/dell/iotnodeapp/MapsActivity.java
@@ -2,10 +2,6 @@ package com.example.dell.iotnodeapp;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -51,46 +47,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        //icon generator
-        mIconGenerator =   new IconGenerator(getApplicationContext());
-        Bitmap icon_marker1 = mIconGenerator.makeIcon("C-Building, room:C5-302" );
-        Bitmap icon_marker2 = mIconGenerator.makeIcon("D-Building, room:D2-209" );
-        Bitmap icon_marker3 = mIconGenerator.makeIcon("B-Building, room:B5-102" );
-        mIconGenerator.setColor(5);
         googleMap.setOnMarkerClickListener(this);
-
+        mIconGenerator =   new IconGenerator(getApplicationContext());
         LatLng GUC = new LatLng(29.9882, 31.4413);
+       for (int i =0; i<5; i++){
 
-        googleMarker = mMap.addMarker((new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(icon_marker1)))
-                        .position(GUC)
+           Bitmap icon_marker1 = mIconGenerator.makeIcon("C-Building, room:C5-302" );
+           googleMarker = mMap.addMarker((new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(icon_marker1)))
+                   .position(new LatLng(29.9882+i, 31.4413+i))
+           );
+          // onMarkerClick(googleMarker);
+       }
+//
+//        Bitmap icon_marker1 = mIconGenerator.makeIcon("C-Building, room:C5-302" );
+//        Bitmap icon_marker2 = mIconGenerator.makeIcon("D-Building, room:D2-209" );
+//        Bitmap icon_marker3 = mIconGenerator.makeIcon("B-Building, room:B5-102" );
 
 
-        );
-         googleMarker2 = mMap.addMarker((new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(icon_marker2)))
-                         .position(new LatLng(29.988, 31.4413))
 
-         );
-         googleMarker3 = mMap.addMarker(((new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(icon_marker3))))
-                         .position(new LatLng(29.9882, 31.441))
 
-       );
+
+//        googleMarker = mMap.addMarker((new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(icon_marker1)))
+//                        .position(GUC)
+//
+//
+//        );
+//         googleMarker2 = mMap.addMarker((new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(icon_marker2)))
+//                         .position(new LatLng(29.988, 31.4413))
+//
+//         );
+//         googleMarker3 = mMap.addMarker(((new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(icon_marker3))))
+//                         .position(new LatLng(29.9882, 31.441))
+//
+//       );
 //        googleMarker3.showInfoWindow();
 //        googleMarker.showInfoWindow();
 //        googleMarker2.showInfoWindow();
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(GUC));
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        if (marker.equals(googleMarker) || marker.equals(googleMarker2) || marker.equals(googleMarker3) ) {
+        //if (marker.equals(googleMarker) || marker.equals(googleMarker2) || marker.equals(googleMarker3) ) {
             //handle click here
             Intent intent = new Intent("com.example.dell.iotnodeapp.sensors");
             startActivity(intent);
-        }
+       // }
         return true;
     }
 
