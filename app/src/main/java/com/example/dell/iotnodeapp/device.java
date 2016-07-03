e268a00633a21414d5798f90732966f8ee7aaf93
safa-ads <safa.mohamed.ads@gmail.com>
2016-07-03 03:04:30 +0300

    Spinner added to device page

--- app/src/main/java/com/example/dell/iotnodeapp/device.java
+++ app/src/main/java/com/example/dell/iotnodeapp/device.java
@@ -8,18 +8,39 @@ import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.SpannableString;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class device extends AppCompatActivity {
+import java.util.ArrayList;
 
-    String edit="<u>This text will be underlined</u>";
-    String udata="Underlined Text";
+public class device extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static Button btn_editcomm;
+    private static TextView comm_device;
+
+    Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        spinner = (Spinner) findViewById(R.id.spinner);
+        editcomm_btn();
+        spinner.setOnItemSelectedListener(this);
+        ArrayList categories = new ArrayList<String>();
+        categories.add("BLE");
+        categories.add("WIFI");
+        categories.add("Xgbee");
+        ArrayAdapter <String> dataAdapter = new ArrayAdapter <String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,4 +51,30 @@ public class device extends AppCompatActivity {
         });
     }
 
+
+    public void editcomm_btn() {
+        btn_editcomm = (Button) findViewById(R.id.btn_commdevice);
+        btn_editcomm.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        ((Spinner) findViewById(R.id.spinner)).performClick();
+                        }
+                    }
+
+            );
+        }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        comm_device = (TextView)findViewById(R.id.commdevice);
+        comm_device.setText("Communicating device: " + item.toString());
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
+
